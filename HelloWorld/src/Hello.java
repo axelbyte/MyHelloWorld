6d9196e310213eb764f1e96de50b8a84896573db
Alejandro Ibarra <axelbyte@hotmail.com>
2015-04-05 00:57:23 +0200

    El menu pasa a ser una variable miembro de la clase Hello

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,10 @@
 
 public class Hello {
+	private static Menu mMenu;
+	public Hello()
+	{
+		mMenu = new Menu();
+	}
 	public static void main (String[] args){
 		if (args.length > 0) {
 			System.out.printf("Hello World %s ",args[0]);
@@ -7,5 +12,7 @@ public class Hello {
 		else {
 			System.out.println("Hello World ");
 		}
+		mMenu.ShowMain();
 	}
+	
 }
