a4769a8f9e6871dc788005e973952529ca8b3216
Alejandro Ibarra <axelbyte@hotmail.com>
2015-04-05 01:52:57 +0200

    Revert "El menu pasa a ser una variable miembro de la clase Hello"
    This reverts commit 6d9196e310213eb764f1e96de50b8a84896573db.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,10 +1,5 @@
 
 public class Hello {
-	private static Menu mMenu;
-	public Hello()
-	{
-		mMenu = new Menu();
-	}
 	public static void main (String[] args){
 		if (args.length > 0) {
 			System.out.printf("Hello World %s ",args[0]);
@@ -12,7 +7,5 @@ public class Hello {
 		else {
 			System.out.println("Hello World ");
 		}
-		mMenu.ShowMain();
 	}
-	
 }
