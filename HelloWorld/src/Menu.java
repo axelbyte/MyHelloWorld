314b63d4aabb21e15dc51114bbfd2fc44cc97275
Alejandro Ibarra <axelbyte@hotmail.com>
2015-04-04 20:46:19 +0200

    Nueva clase Menu primera implementacion

---
+++ HelloWorld/src/Menu.java
@@ -0,0 +1,29 @@
+import java.util.function.Function;
+
+
+public class Menu {
+	public void ShowMain (){
+		System.out.println("***********************");
+		System.out.println("*   Menu principal    *");
+		System.out.println("***********************");
+		System.out.println("* 1) opcion 1         *");
+		System.out.println("* 2) opcion 2         *");
+		System.out.println("* 3) opcion 3         *");
+		System.out.println("***********************");
+	}
+	public void ShowOpt1 (){
+		System.out.println("***********************");
+		System.out.println("*   Opcion 1          *");
+		System.out.println("***********************");
+	}
+	public void ShowOpt2 (){
+		System.out.println("***********************");
+		System.out.println("*   Opcion 2          *");
+		System.out.println("***********************");
+	}
+	public void ShowOpt3 (){
+		System.out.println("***********************");
+		System.out.println("*   Opcion 3          *");
+		System.out.println("***********************");
+	}
+}
